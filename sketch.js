3b376388b71ddfb36b7d5bf4a6285b4ed1634e81
selfmademuskaan <selfmademuskaan@gmail.com>
2020-09-10 17:56:44 +0530

    pif4 added

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var pig4;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -27,7 +28,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
-
+pig4=new Pig(870,150)
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
@@ -49,7 +50,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+pig4.display();
     box3.display();
     box4.display();
     pig3.display();
